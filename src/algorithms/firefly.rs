8e286be466d76759f5447382ea4e46cbe42b6ec3
Aljaz <aljazsustar99@gmail.com>
2023-05-14 20:10:44 +0200

    Base implementation of simulated annealing

--- src/algorithms/firefly.rs
+++ src/algorithms/firefly.rs
@@ -2,6 +2,6 @@ use super::common::Minimum;
 
 use miette::{Result, miette};
 
-pub fn run_firefly<'a>() -> Result<Minimum<'a>> {
+pub fn run_firefly<'a>() -> Result<Minimum> {
     Err(miette!("Not implemented!"))
 }
