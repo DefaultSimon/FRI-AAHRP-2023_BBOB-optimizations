5d1dc6084b4f09f78d2f767cb75664b294cfb2c2
Simon Goričar <simon.peter.goricar@gmail.com>
2023-05-14 11:45:23 +0200

    Improve `valide_functions.rs` - now with seeded randomness and 240 samples

--- smoof_comparison.R
+++ smoof_comparison.R
@@ -1,16 +1,35 @@
-# Install the smoof package if not installed yet, then load.
+# Install the smoof package if not installed yet, then load it.
 if (!is.element("smoof", installed.packages()[,1])) {
     install.packages("smoof")
 }
 library("smoof")
 
-print("The output of this script should be compared with the output of `cargo run -- run-R-smoof-comparison`.")
-print("Calculating all 24 BBOB functions at point [4, 4, 4, ...] (4 in all 40 dimensions).")
+# Configuration and seeding
+set.seed(2932921)
+config.samplesPerFunction <- 10
 
-# Calculate the functions and print them out.
-print("-- BEGIN --")
+
+cat(
+  "Do not run this script standalone, run `cargo run --bin validate_functions` instead.\n"
+)
+cat(sprintf(
+  "Sampling all 24 BBOB functions %.0f times each at random (seeded) points.\n",
+  config.samplesPerFunction
+))
+
+# Sample the functions and prints results.
+
+cat("-- BEGIN --\n")
 for (functionIndex in 1:24) {
-    value <- makeBBOBFunction(40, functionIndex, 2023)(rep(4, times = 40))
-    print(sprintf("Function %g: %s", functionIndex, toString(value)))
+    for (sampleIndex in 1:config.samplesPerFunction) {
+      inputParameters = runif(40, min = -5, max = 5)
+      formattedInputParameters = paste(inputParameters, sep = ",", collapse = ",")
+      
+      value <- makeBBOBFunction(40, functionIndex, 2023)(inputParameters)
+      cat(sprintf(
+        "bbob_function_index=%.0f;parameters=[%s];value=%f\n",
+        functionIndex, formattedInputParameters, value
+      ))
+    }
 }
-print("-- END --")
+cat("-- END --\n")
