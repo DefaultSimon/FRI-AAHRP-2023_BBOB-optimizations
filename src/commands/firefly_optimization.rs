0908e29492a71e3327a5cbee994aa9a8e58931e3
Simon Goričar <simon.peter.goricar@gmail.com>
2023-05-16 15:32:17 +0200

    Explicitly configure rayon's global thread pool

--- src/commands/firefly_optimization.rs
+++ src/commands/firefly_optimization.rs
@@ -1,25 +1,45 @@
+use std::thread;
 use std::time::Instant;
 
-use miette::Result;
+use miette::{miette, IntoDiagnostic, Result, WrapErr};
+use rayon::ThreadPoolBuilder;
 
 use crate::algorithms::firefly::perform_firefly_swarm_optimization;
 use crate::core::names::ALL_BBOB_FUNCTION_NAMES;
 use crate::core::suite::BBOBSuite;
 
 pub fn cmd_run_firefly_optimization() -> Result<()> {
-    let start_time = Instant::now();
+    // Make sure we're using as many cores as we can.
+    ThreadPoolBuilder::new()
+        .num_threads(
+            thread::available_parallelism()
+                .into_diagnostic()
+                .wrap_err_with(|| {
+                    miette!("Could not get available parallelism.")
+                })?
+                .into(),
+        )
+        .build_global()
+        .into_diagnostic()
+        .wrap_err_with(|| {
+            miette!("Could not modify rayon's global thread pool.")
+        })?;
 
+    // Initialize coco / the BBOB suite.
     let mut suite = BBOBSuite::new()?;
+
+    let start_time = Instant::now();
 
     for problem_name in ALL_BBOB_FUNCTION_NAMES {
         let problem = suite.problem(problem_name, None)?;
 
         let minimum = perform_firefly_swarm_optimization(problem, None)?;
         println!(
-            "[{:02}/{:02}|{}] Minimum: {:.6}",
+            "[{:02}/{:02}|{}] {}Minimum: {:.6}",
             problem_name.to_function_index(),
             ALL_BBOB_FUNCTION_NAMES.len(),
             problem_name.to_function_name(),
+            " ".repeat(32 - problem_name.to_function_name().len()),
             minimum.value,
         );
     }
